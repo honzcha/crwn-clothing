d91ed51a2c1fa7326f927c0ca9fcb4e95058fbce
honschaa <45900433+honschaa@users.noreply.github.com>
2020-07-22 00:50:11 +0800

    added styles to the page, split objects into components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./App.css";
 
-import HomePage from "./homepage.component";
-import "./homepage.styles.scss";
+import HomePage from "./pages/homepage/homepage.component";
+import "./pages/homepage/homepage.styles.scss";
 
 function App() {
   return (
