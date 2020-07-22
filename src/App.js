3673d9c6cd928428d470cfe710b15df0f6ce9396
honschaa <45900433+honschaa@users.noreply.github.com>
2020-07-22 15:47:02 +0800

    added routing and better styling for homepage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
 import "./pages/homepage/homepage.styles.scss";
 
+const HatsPage = () => (
+  <div>
+    <h1>Item Page</h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
