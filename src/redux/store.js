27dbb91b9d8a77fe2a06ecb5b038e71b2ae25d5a
Lucas Garcia <honzcha@outlook.com>
2020-08-24 15:56:28 +0800

    set logger for dif dev env and prod env

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,15 @@
-import { createStore, applyMiddleware } from 'redux';
-import { persistStore } from 'redux-persist';
-import logger from 'redux-logger';
+import { createStore, applyMiddleware } from "redux";
+import { persistStore } from "redux-persist";
+import logger from "redux-logger";
 
-import rootReducer from './root-reducer';
+import rootReducer from "./root-reducer";
 
-const middlewares = [logger];
+const middlewares = [];
+
+// this will do that when we run "yarn build" it will not push the logger into the middleware array, however while we work in the localhost it will push the logger into the array and allow for debugging.
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
